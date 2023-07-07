b004814e5dc3431e0bc16f952c10645c929a1c03
italotss <italotsv@gmail.com>
2023-07-06 21:17:13 -0300

    coloquei campos na framedata

--- src/modules/framedata/framedata.module.ts
+++ src/modules/framedata/framedata.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FramedataService } from './framedata.service';
 import { FramedataController } from './framedata.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Framedata } from './entities/framedatum.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Framedata])],
   controllers: [FramedataController],
   providers: [FramedataService]
 })
